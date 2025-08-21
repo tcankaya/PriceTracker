ca51e4555dd0870afa86bad56a27d8a9bf42faf2
Talha Cankaya <142382707+tcankaya@users.noreply.github.com>
2025-08-22 01:13:01 +0300

    Update Notifier.java

--- src/main/java/app/Notifier.java
+++ src/main/java/app/Notifier.java
@@ -4,6 +4,5 @@ public class Notifier {
     public void sendAlert(Product p, double oldPrice, double newPrice) {
         System.out.println("🔥 Price drop detected for " + p.getBrand() + "!");
         System.out.println("Old price: " + oldPrice + " → New price: " + newPrice);
-        // You can extend this: send email, Telegram, or push notification
     }
 }
